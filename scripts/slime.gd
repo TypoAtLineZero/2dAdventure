930bdeba4a62bf054699d81265e6b4136c92256c
franz <fneumeister@in-technology.eu>
2024-05-10 00:28:49 +0200

    slime is flipping horizontaly

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -7,12 +7,15 @@ var direction = 1
 # Adding as reference by hitting control while drag+drop
 @onready var raycast_right = $RaycastRight
 @onready var raycast_left = $RaycastLeft
+@onready var animated_sprite = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if raycast_right.is_colliding():
 		direction = -1
+		animated_sprite.flip_h = true
 	if raycast_left.is_colliding():
 		direction = 1
+		animated_sprite.flip_h = false
 
 	position.x += direction * SPEED * delta
