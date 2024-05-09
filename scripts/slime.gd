7bd67f23ed6b51ab95e38fa19b1a286848f38007
franz <fneumeister@in-technology.eu>
2024-05-10 00:25:23 +0200

    Adding looping movement for slime

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -4,7 +4,15 @@ const SPEED = 60
 
 var direction = 1
 
+# Adding as reference by hitting control while drag+drop
+@onready var raycast_right = $RaycastRight
+@onready var raycast_left = $RaycastLeft
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if raycast_right.is_colliding():
+		direction = -1
+	if raycast_left.is_colliding():
+		direction = 1
+
 	position.x += direction * SPEED * delta
