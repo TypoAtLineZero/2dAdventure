eade5c6fab3102102e0358b0eb3b46bf69c967a4
franz <fneumeister@in-technology.eu>
2024-05-18 23:29:08 +0200

    Adding CanvasLayer for current score

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,7 +2,9 @@ extends Node
 
 var score = 0
 @onready var score_label = $ScoreLabel
+@onready var canvas_label = %CanvasLabel
 
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins!"
+	canvas_label.text = str(score) + " coins"
