7b314481e3a40286fc550ce3c7b19dce2c0c44e5
franz <fneumeister@in-technology.eu>
2024-05-19 00:24:22 +0200

    WIP - health display (reset behaviour unclear after scene reload)

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,9 +2,9 @@ extends Node
 
 var score = 0
 @onready var score_label = $ScoreLabel
-@onready var canvas_label = %CanvasLabel
+@onready var canvas_label_score = %CanvasLabel_Score
 
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins!"
-	canvas_label.text = str(score) + " coins"
+	canvas_label_score.text = str(score) + " coins"
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,9 +1,20 @@
 extends Area2D
 
+var health: int = 3: set = _set_health, get = _get_health
 @onready var timer = $Timer
+@onready var canvas_label_health = %CanvasLabel_Health
+
+func _set_health(reduce_health: int) -> void:
+	health += reduce_health
+
+func _get_health() -> int:
+	return health
 
 func _on_body_entered(body):
+	#health -= 1
+	_set_health(-1)
 	print("You died.")
+	canvas_label_health.text = str(health) + " lifes left"
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
@@ -12,3 +23,4 @@ func _on_body_entered(body):
 func _on_timer_timeout():
 	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
+	health = _get_health()
